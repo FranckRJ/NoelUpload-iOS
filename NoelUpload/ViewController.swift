342318f66009c7cfba0f8f9a3f5547af7ae6e80d
Franck W <franck.rj.w@gmail.com>
2019-06-19 15:11:33 +0200

    Maj du gitignore et ajout des fichiers par defaut d'un projet xcode ios.

---
+++ NoelUpload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoelUpload
+//
+//  Created by Franck WERNER on 6/19/19.
+//  Copyright © 2019 FranckRJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
