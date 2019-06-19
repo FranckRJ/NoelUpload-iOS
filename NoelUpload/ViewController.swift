6a43228c7c2e71a348aebe6ec2b626959e8ff87c
Franck W <franck.rj.w@gmail.com>
2019-06-19 16:28:03 +0200

    Ajout de la selection d'image a upload.

--- NoelUpload/ViewController.swift
+++ NoelUpload/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageToUpload: UIImageView!
+    
+    @IBAction func selectImageTapped(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        imageToUpload.image = selectedImage
+        dismiss(animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadButtonTapped(_ sender: UIButton) {
+        let durationInSec: Double = 5
+        let message = "Upload en cours... (dans le futur)"
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + durationInSec) {
+            alert.dismiss(animated: true, completion: nil)
+        }
+    }
 }
-
